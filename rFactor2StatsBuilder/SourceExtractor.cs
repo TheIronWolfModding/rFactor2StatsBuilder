d341ea6f158b27cde79c2f8491d3a6643dcea1cd
TheIronWolfModding <vleonavicius@hotmail.com>
2017-10-30 09:56:06 -0700

    Extract operation: Add ModMgr calls.

--- rFactor2StatsBuilder/SourceExtractor.cs
+++ rFactor2StatsBuilder/SourceExtractor.cs
@@ -6,6 +6,7 @@ Website: thecrewchief.org
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,7 @@ namespace rFactor2StatsBuilder
 
     private static void ProcessSingleVehicle(string vehFullPath, string vehDir)
     {
-      Utils.WriteLine($"Processing vehicle: {vehDir}", ConsoleColor.Green);
+      Utils.WriteLine($"\nProcessing vehicle: {vehDir}", ConsoleColor.Green);
 
       var vehOutPath = $"{rFactor2StatsBuilder.OutRoot}\\{vehDir}";
       try
@@ -40,11 +41,23 @@ namespace rFactor2StatsBuilder
         throw ex;
       }
 
+      if (Directory.GetDirectories(vehFullPath).Length > 1)
+        Utils.WriteLine($"Warning: {vehDir} contains more than one version folders.", ConsoleColor.Yellow);
+
       foreach (var masFileFull in Directory.GetFiles(vehFullPath, "*.mas", SearchOption.AllDirectories))
       {
-        Utils.WriteLine($"Processing .mas: {masFileFull}", ConsoleColor.Cyan);
+        Utils.WriteLine($"\nProcessing .mas: {masFileFull}", ConsoleColor.Cyan);
 
+        var psi = new ProcessStartInfo(rFactor2StatsBuilder.ModMgr, $"*.veh *.tbc *.hdv -x{masFileFull} -o{vehOutPath}");
+        var process = new Process() { StartInfo = psi };
 
+        process.Start();
+
+        // Not calling for wait is faster, but produce weird console output.  So for now, keep things synchronous.
+        process.WaitForExit();
+
+        // ModMgr does not return error code on failure.  We could possibly parse the console output somehow,
+        // but for now, simply let resolution of things fail during stats build phase.
       }
     }
 
